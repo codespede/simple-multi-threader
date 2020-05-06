802ef07a9bd0ba4c26840640e8659c9d247abba9
Mahesh S Warrier <maheshs60@gmail.com>
2020-05-07 00:50:34 +0530

    laravel support

--- src/Threader.php
+++ src/Threader.php
@@ -4,21 +4,45 @@ use \SuperClosure\Serializer;
 class Threader{
 	public $arguments;
 	public $jobsDir = "smt-jobs";
-	public $logPath = "smt-logs";
+	public $logsDir = "smt-logs";
 	public $nohup = true;
 	
+	public function __construct($config = []){
+        if (!empty($config)) {
+            self::configure($this, $config);
+        }
+        $this->init();
+    }
+
+    public function init(){
+    	if(!file_exists($this->jobsDir))
+    		mkdir($this->jobsDir, 0777);
+    	if(!file_exists($this->logsDir))
+    		mkdir($this->logsDir, 0777);
+    }
+
 	public function thread($closure){
+        //dd(getcwd());
 		$serializer = new Serializer;
 		$jobId = md5(uniqid(rand(), true));
+
 		file_put_contents("{$this->jobsDir}/{$jobId}_closure.ser", $serializer->serialize($closure));
         file_put_contents("{$this->jobsDir}/{$jobId}_arguments.ser", serialize($this->arguments));
-        $command = "php ".__DIR__."/thread.php {$jobId} {$this->jobsDir}";
+        $command = "php ".__DIR__."/thread.php {$jobId} {$this->jobsDir} {$this->logsDir}";
         if(!self::isWindows() && $this->nohup)
         	$command = "nohup {$command} > /dev/null 2>&1 &";
+        dd($command);
 		return shell_exec($command);
 	}
 
 	public static function isWindows(){
 		return strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
 	}
+
+	public static function configure($object, $properties){
+        foreach ($properties as $name => $value) {
+            $object->$name = $value;
+        }
+        return $object;
+    }
 }
--- src/thread.php
+++ src/thread.php
@@ -1,14 +1,76 @@
 <?php
+function getExceptionTraceAsString($exception) {
+    $rtn = "";
+    $count = 0;
+    foreach ($exception->getTrace() as $frame) {
+
+
+        $args = "";
+        if (isset($frame['args'])) {
+            $args = array();
+            foreach ($frame['args'] as $arg) {
+                if (is_string($arg)) {
+                    $args[] = "'" . $arg . "'";
+                } elseif (is_array($arg)) {
+                    $args[] = "Array";
+                } elseif (is_null($arg)) {
+                    $args[] = 'NULL';
+                } elseif (is_bool($arg)) {
+                    $args[] = ($arg) ? "true" : "false";
+                } elseif (is_object($arg)) {
+                    $args[] = get_class($arg);
+                } elseif (is_resource($arg)) {
+                    $args[] = get_resource_type($arg);
+                } else {
+                    $args[] = $arg;
+                }
+            }
+            $args = join(", ", $args);
+        }
+        $current_file = "[internal function]";
+        if(isset($frame['file']))
+        {
+            $current_file = $frame['file'];
+        }
+        $current_line = "";
+        if(isset($frame['line']))
+        {
+            $current_line = $frame['line'];
+        }
+        $rtn .= sprintf( "#%s %s(%s): %s(%s)\n",
+            $count,
+            $current_file,
+            $current_line,
+            $frame['function'],
+            $args );
+        $count++;
+    }
+    return $rtn;
+}
 use SuperClosure\Serializer;
-require('vendor/autoload.php');
+require('../vendor/autoload.php');
 $jobId = $argv[1];
 $jobsDir = $argv[2];
+$logsDir = $argv[3];
 if(!file_exists("{$jobsDir}/{$jobId}_closure.ser"))
-	die("Closure file for Job ID: $jobId doesn't exist");
+    die("Closure file for Job ID: $jobId doesn't exist");
 if(!file_exists("{$jobsDir}/{$jobId}_arguments.ser"))
-	die("Arguments file for Job ID: $jobId doesn't exist");
+    die("Arguments file for Job ID: $jobId doesn't exist");
 $serializer = new Serializer;
-$closure = $serializer->unserialize(file_get_contents("{$jobsDir}/{$jobId}_closure.ser"));
-$arguments = unserialize(file_get_contents("{$jobsDir}/{$jobId}_arguments.ser"));
-file_put_contents("smt_{$jobId}.log", $closure($arguments));
+try{
+    $closure = $serializer->unserialize(file_get_contents("{$jobsDir}/{$jobId}_closure.ser"));
+    $arguments = unserialize(file_get_contents("{$jobsDir}/{$jobId}_arguments.ser"));
+    $app = require_once '../bootstrap/app.php';
+    $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+    $status = $kernel->handle(
+        $input = new Symfony\Component\Console\Input\ArgvInput,
+        new Symfony\Component\Console\Output\ConsoleOutput
+    );
+    file_put_contents("{$logsDir}/smt_{$jobId}.log", $closure($arguments));
+}catch(\Exception $e){
+    file_put_contents("{$logsDir}/smt_{$jobId}_error.log", "Caught Exception at ".date('Y-m-d H:i:s').": ".$e->getMessage()." at line: ".$e->getLine()." on file: ".$e->getFile().". Stack trace: ".getExceptionTraceAsString($e));
+}
+//garbage collection..
+//unlink("{$jobsDir}/{$jobId}_closure.ser");
+//unlink("{$jobsDir}/{$jobId}_arguments.ser");
 exit;
